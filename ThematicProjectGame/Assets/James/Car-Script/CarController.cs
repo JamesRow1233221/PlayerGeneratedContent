2689f3b900ab097b71a4da54c875bf3fae39dead
JamesRowb <145841339+JamesRow1233221@users.noreply.github.com>
2026-02-19 12:14:00 +0000

    Better Car made

---
+++ ThematicProjectGame/Assets/James/Car-Script/CarController.cs
@@ -0,0 +1,69 @@
+using UnityEditor.ShaderGraph;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CarController : MonoBehaviour
+{
+    public float fwdSpeed;
+    public float revSpeed;
+    public float turnSpeed;
+
+    private float moveInput;
+    private float turnInput;
+    private bool isCarGrounded;
+
+    private float normalDrag;
+    public float modifiedDrag;
+
+    public Rigidbody sphereRB;
+
+    public LayerMask groundLayer;
+
+    public float alignToGroundTime;
+
+    void Start()
+    {
+        sphereRB.transform.parent = null;
+
+        normalDrag = sphereRB.linearDamping;
+    }
+
+    void Update()
+    {
+        moveInput = Input.GetAxisRaw("Vertical");
+        turnInput = Input.GetAxisRaw("Horizontal");
+
+        float newRot = turnInput * turnSpeed * Time.deltaTime * moveInput;
+
+        if (isCarGrounded)
+        {
+            transform.Rotate(0, newRot, 0, Space.World);
+        }
+    
+        transform.position = sphereRB.transform.position;
+
+        
+
+        RaycastHit hit;
+        isCarGrounded = Physics.Raycast(transform.position, -transform.up, out hit, 1f, groundLayer);
+
+        Quaternion toRotateTo = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+        transform.rotation = Quaternion.Slerp(transform.rotation, toRotateTo, alignToGroundTime * Time.deltaTime);
+
+        moveInput *= moveInput > 0 ? fwdSpeed : revSpeed;
+
+        sphereRB.linearDamping = isCarGrounded ? normalDrag : modifiedDrag;
+    }
+
+    private void FixedUpdate()
+    {
+        if (isCarGrounded)
+        {
+            sphereRB.AddForce(transform.forward * moveInput, ForceMode.Acceleration);
+        }
+        else
+        {
+            sphereRB.AddForce(transform.up * -40f);
+        }
+    }
+}
