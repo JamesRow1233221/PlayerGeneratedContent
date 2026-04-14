dce84f0008b596e1ec847516291651f5500d6bb2
JamesRowb <145841339+JamesRow1233221@users.noreply.github.com>
2026-04-14 16:40:01 +0100

    working on trackconnecting

--- ThematicProjectGame/Assets/James/Track-Scripts/TrackConnecting.cs
+++ ThematicProjectGame/Assets/James/Track-Scripts/TrackConnecting.cs
@@ -1,173 +1,148 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TrackConnecting : MonoBehaviour
 {
-    [Header("Grid Settings")]
-    public float gridSize = 10f; // Size of one grid cell
-    
-    [Header("Connection Points")]
-    public TrackConnectionSide[] availableSides; // Which sides can connect (North, South, East, West)
-    
-    [Header("Visuals")]
-    public Material validPlacementMaterial;
-    public Material invalidPlacementMaterial;
-    public Material normalMaterial;
-    
-    private Vector3 gridPosition;
-    private bool isDragging = false;
-    private bool canPlace = true;
-    private Camera mainCamera;
-    private Renderer trackRenderer;
-    private Collider trackCollider;
-    private Vector3 dragOffset;
-    
-    void Start()
+    [System.Serializable]
+    public class TrackType
     {
-        mainCamera = Camera.main;
-        trackRenderer = GetComponent<Renderer>();
-        trackCollider = GetComponent<Collider>();
-        
-        if (normalMaterial == null && trackRenderer != null)
+        public string name;
+        public GameObject prefab;
+    }
+
+    public TrackType[] trackTypes;
+    public float gridSize = 1f;
+    private int currentTrackIndex = 0;
+    private GameObject ghostObject;
+    private HashSet<Vector3> occupiedPositions = new HashSet<Vector3>();
+
+    private void Start()
+    {
+        if(trackTypes.Length > 0)
         {
-            normalMaterial = trackRenderer.material;
+            CreateGhostObject();
+        }
+        else
+        {
+            Debug.LogError("No track types assigned to TrackConnecting!");
         }
     }
 
-    void Update()
+    private void Update()
     {
-        if (isDragging)
+        UpdateGhostPosition();
+        if(Input.GetMouseButtonDown(0))
         {
-            DragTrack();
-            CheckPlacementValidity();
+            PlaceObject();
         }
     }
-    
-    void OnMouseDown()
+
+    public void SelectTrackType(int index)
     {
-        isDragging = true;
-        
-        // Calculate offset between mouse position and object position
-        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
+        if(index >= 0 && index < trackTypes.Length)
         {
-            dragOffset = transform.position - hit.point;
-        }
-        
-        // Remove from grid if already placed
-        if (TrackGridManager.Instance != null)
-        {
-            TrackGridManager.Instance.RemoveTrackFromGrid(this);
+            currentTrackIndex = index;
+            DestroyGhostObject();
+            CreateGhostObject();
         }
     }
-    
-    void OnMouseDrag()
+
+    public TrackType[] GetTrackTypes()
     {
-        isDragging = true;
+        return trackTypes;
     }
-    
-    void OnMouseUp()
+
+    void CreateGhostObject()
     {
-        if (isDragging)
+        if(trackTypes[currentTrackIndex].prefab == null)
         {
-            isDragging = false;
-            
-            if (canPlace && TrackGridManager.Instance != null)
-            {
-                // Snap to grid and register
-                transform.position = gridPosition;
-                TrackGridManager.Instance.PlaceTrackOnGrid(this, gridPosition);
-                
-                // Reset material
-                if (trackRenderer != null && normalMaterial != null)
-                {
-                    trackRenderer.material = normalMaterial;
-                }
-            }
-            else
-            {
-                // Invalid placement - could implement return to original position or destroy
-                Debug.Log("Invalid placement! Track overlaps with existing track.");
-            }
-        }
-    }
-    
-    void DragTrack()
-    {
-        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        
-        // Raycast to a ground plane or use a fixed Y position
-        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
-        float distance;
-        
-        if (groundPlane.Raycast(ray, out distance))
-        {
-            Vector3 worldPosition = ray.GetPoint(distance) + dragOffset;
-            
-            // Snap to grid
-            gridPosition = SnapToGrid(worldPosition);
-            transform.position = gridPosition;
-        }
-    }
-    
-    Vector3 SnapToGrid(Vector3 position)
-    {
-        float x = Mathf.Round(position.x / gridSize) * gridSize;
-        float z = Mathf.Round(position.z / gridSize) * gridSize;
-        
-        return new Vector3(x, position.y, z);
-    }
-    
-    void CheckPlacementValidity()
-    {
-        if (TrackGridManager.Instance == null)
-        {
-            canPlace = true;
+            Debug.LogError($"Track type '{trackTypes[currentTrackIndex].name}' has no prefab assigned!");
             return;
         }
-        
-        canPlace = !TrackGridManager.Instance.IsPositionOccupied(gridPosition, this);
-        
-        // Update visual feedback
-        if (trackRenderer != null)
+
+        ghostObject = Instantiate(trackTypes[currentTrackIndex].prefab);
+        ghostObject.GetComponent<Collider>().enabled = false;
+
+        Renderer[] renderers = ghostObject.GetComponentsInChildren<Renderer>();
+
+        foreach (Renderer renderer in renderers)
         {
-            if (canPlace && validPlacementMaterial != null)
-            {
-                trackRenderer.material = validPlacementMaterial;
-            }
-            else if (!canPlace && invalidPlacementMaterial != null)
-            {
-                trackRenderer.material = invalidPlacementMaterial;
-            }
+            Material mat = renderer.material;
+            Color color = mat.color;    
+            color.a = 0.5f; 
+            mat.color = color;
+
+            mat.SetFloat("_Mode", 2);
+            mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+            mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+            mat.SetInt("_ZWrite", 0);
+            mat.DisableKeyword("_ALPHATEST_ON");
+            mat.EnableKeyword("_ALPHABLEND_ON");
+            mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+            mat.renderQueue = 3000;
         }
     }
-    
-    public Vector3 GetGridPosition()
-    {
-        return gridPosition;
-    }
-    
-    public TrackConnectionSide[] GetAvailableSides()
-    {
-        return availableSides;
-    }
-    
-    public Vector3Int GetGridCoordinates()
-    {
-        return new Vector3Int(
-            Mathf.RoundToInt(gridPosition.x / gridSize),
-            0,
-            Mathf.RoundToInt(gridPosition.z / gridSize)
-        );
-    }
-}
 
-[System.Serializable]
-public enum TrackConnectionSide
-{
-    North,
-    South,
-    East,
-    West
+    void DestroyGhostObject()
+    {
+        if(ghostObject != null)
+        {
+            Destroy(ghostObject);
+        }
+    }
+
+    void UpdateGhostPosition()
+    {
+        if(ghostObject == null)
+            return;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(ray, out RaycastHit hit))
+        {
+            Vector3 point = hit.point;
+
+            Vector3 snappedPosition = new Vector3(
+                Mathf.Round(hit.point.x / gridSize) * gridSize,
+                Mathf.Round(hit.point.y / gridSize) * gridSize,
+                Mathf.Round(hit.point.z / gridSize) * gridSize
+            );
+            ghostObject.transform.position = snappedPosition;
+
+            if (occupiedPositions.Contains(snappedPosition))
+                SetGhostColor(Color.red);
+            else
+                SetGhostColor(new Color(1f, 1f, 1f, 0.5f));
+        }
+    }
+
+    void SetGhostColor(Color color)
+    {
+        Renderer[] renderers = ghostObject.GetComponentsInChildren<Renderer>();
+        foreach (Renderer renderer in renderers)
+        {
+            Material mat = renderer.material;
+            mat.color = color;
+        }
+    }
+
+    void PlaceObject()
+    {
+        if(ghostObject == null)
+            return;
+
+        Vector3 placementPosition = ghostObject.transform.position;
+
+        if(!occupiedPositions.Contains(placementPosition))
+        {
+            Instantiate(trackTypes[currentTrackIndex].prefab, placementPosition, Quaternion.identity);
+            occupiedPositions.Add(placementPosition);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        DestroyGhostObject();
+    }
 }
---
+++ ThematicProjectGame/Assets/James/Track-Scripts/TrackSelectionUI.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TrackSelectionUI : MonoBehaviour
+{
+    public TrackConnecting trackConnecting;
+    public Transform buttonContainer;
+    public GameObject buttonPrefab;
+    public Color selectedColor = Color.green;
+    public Color normalColor = Color.white;
+
+    private List<Button> trackButtons = new List<Button>();
+    private int currentSelectedIndex = 0;
+
+    private void Start()
+    {
+        if(trackConnecting == null)
+        {
+            Debug.LogError("TrackConnecting reference not assigned!");
+            return;
+        }
+
+        GenerateTrackButtons();
+    }
+
+    void GenerateTrackButtons()
+    {
+        TrackConnecting.TrackType[] trackTypes = trackConnecting.GetTrackTypes();
+
+        for(int i = 0; i < trackTypes.Length; i++)
+        {
+            GameObject buttonObj = Instantiate(buttonPrefab, buttonContainer);
+            Button button = buttonObj.GetComponent<Button>();
+            Text buttonText = buttonObj.GetComponentInChildren<Text>();
+
+            if(buttonText != null)
+            {
+                buttonText.text = trackTypes[i].name;
+            }
+
+            int index = i;
+            button.onClick.AddListener(() => OnTrackButtonClicked(index));
+
+            trackButtons.Add(button);
+        }
+
+        UpdateButtonColors();
+    }
+
+    void OnTrackButtonClicked(int index)
+    {
+        currentSelectedIndex = index;
+        trackConnecting.SelectTrackType(index);
+        UpdateButtonColors();
+    }
+
+    void UpdateButtonColors()
+    {
+        for(int i = 0; i < trackButtons.Count; i++)
+        {
+            ColorBlock colors = trackButtons[i].colors;
+            colors.normalColor = (i == currentSelectedIndex) ? selectedColor : normalColor;
+            trackButtons[i].colors = colors;
+        }
+    }
+}
+    
