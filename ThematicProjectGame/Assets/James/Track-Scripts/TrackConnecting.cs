f7bbae2b47ec4a062f1ee30a9ebe88b177ae831d
aida-jelly <gameraidk10@gmail.com>
2026-04-27 23:34:06 +0100

    Bug Fix
    - Fixed bug where track with track id over 4 would be placed in the wrong place

--- ThematicProjectGame/Assets/James/Track-Scripts/TrackConnecting.cs
+++ ThematicProjectGame/Assets/James/Track-Scripts/TrackConnecting.cs
@@ -133,7 +133,7 @@ public class TrackConnecting : MonoBehaviour
             );
 
 
-            if(currentTrackIndex == 4)
+            if(currentTrackIndex >= 4)
             {
                 snappedPosition += ghostObject.transform.forward * (-gridSize / 2f);
             }
