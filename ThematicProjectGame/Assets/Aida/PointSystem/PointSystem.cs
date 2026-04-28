9289f3f1899fb9220127eed47868e59c57fe566c
aida-jelly <gameraidk10@gmail.com>
2026-04-28 16:11:30 +0100

    HillPrefab colliders

--- ThematicProjectGame/Assets/Aida/PointSystem/PointSystem.cs
+++ ThematicProjectGame/Assets/Aida/PointSystem/PointSystem.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class PointSystem : MonoBehaviour
 {
+    [SerializeField] private TrackConnecting trackConnecting;
+    [SerializeField] private GameObject finishPrefab;
+    private GameObject finishTrack;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,16 @@ public class PointSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            PlaceFinishTrack();
+        }
+    }
+
+    public void PlaceFinishTrack()
+    {
+        Transform lastPos = trackConnecting.lastPlacedTrack;
+        Instantiate(finishPrefab,lastPos.position + lastPos.transform.forward * 20f,lastPos.rotation);
+
     }
 }
