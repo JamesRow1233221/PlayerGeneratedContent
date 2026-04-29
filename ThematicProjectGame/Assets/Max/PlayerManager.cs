3f2e0abf1722fe3a47bad5338dd18da0283e5cd7
JamesRowb <145841339+JamesRow1233221@users.noreply.github.com>
2026-04-29 16:02:03 +0100

    THANK GOD

--- ThematicProjectGame/Assets/Max/PlayerManager.cs
+++ ThematicProjectGame/Assets/Max/PlayerManager.cs
@@ -1,4 +1,6 @@
+using System.Linq;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerManager : MonoBehaviour
 {
@@ -8,9 +10,23 @@ public class PlayerManager : MonoBehaviour
 
     private void Awake()
     {
+        var devices = InputSystem.devices.OfType<Gamepad>().ToList();
+
         for (int i = 0; i < connectedPlayers; i++)
         {
-            GameObject car = Instantiate(cars[i], spawnPoints[i].position, spawnPoints[i].rotation);
+            if (i >= devices.Count)
+            {
+                Debug.LogWarning($"Not enough gamepads connected for player {i + 1}. Expected {connectedPlayers}, but found {devices.Count}.");
+            }
+            else
+            {
+                Debug.Log($"Player {i + 1} assigned to gamepad: {devices[i].displayName}");
+
+                GameObject car = Instantiate(cars[i], spawnPoints[i].position, spawnPoints[i].rotation);
+
+                var playerInput = car.GetComponent<PlayerInput>();
+                playerInput.SwitchCurrentControlScheme("Gamepad", devices[i]);
+            }
         }
     }
 }
